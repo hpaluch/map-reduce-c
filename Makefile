667fe0ff255904eba901be97260845cde415dec6
Henryk Paluch <hpaluch@seznam.cz>
2015-06-29 21:09:19 +0200

    Single threaded version

--- Makefile
+++ Makefile
@@ -1,6 +1,12 @@
 CFLAGS = -Wall -g -pthread
 LDFLAGS = -g -pthread
 
+# use 'make run RUN_THREADS=X' to override
+# temporarily set to 0 (unless thread version is implemented)
+RUN_THREADS = 0
+# input file to count words - use 'make run WORDS_FILE=my_file.txt' to override
+WORDS_FILE  = /var/cache/man/whatis
+
 all : mapr_wc mtrace_mapr_wc
 
 mapr_wc: mapr_wc.o
@@ -13,11 +19,11 @@ mtrace_mapr_wc.o : mapr_wc.c
 
 .PHONY: run
 run: mapr_wc
-	./mapr_wc 2
+	./mapr_wc $(WORDS_FILE) $(RUN_THREADS)
 
 .PHONY: mtrace_run
 mtrace_run: mtrace_mapr_wc
-	MALLOC_TRACE=trace_mem.lst ./mtrace_mapr_wc 2
+	MALLOC_TRACE=trace_mem.lst ./mtrace_mapr_wc $(WORDS_FILE) $(RUN_THREADS)
 	mtrace mtrace_mapr_wc trace_mem.lst
 
 .PHONY: clean
--- mapr_wc.c
+++ mapr_wc.c
@@ -5,6 +5,7 @@
 #include<stdio.h>
 #include<errno.h>
 #include<stdlib.h>
+#include<string.h>
 
 #include<pthread.h>
 
@@ -15,15 +16,26 @@
 #include <mcheck.h>
 #endif
 
-int usage(char *prog, char *msg){
+static int usage(char *prog, char *msg){
     if(msg != NULL ){
        fprintf(stderr,"ERROR: %s\n",msg);
     }
-    fprintf(stderr,"Usage: %s <number_of_threads>\n",prog);
+    fprintf(stderr,"Usage: %s <file_to_wordcount> <number_of_threads>\n",prog);
     return 1;
 }
 
-void *map_thread(void *arg){
+static int count_words(char *line){
+       char *state;
+       int wc = 0; /* word count */
+       while ( strtok_r(line,"\t ",&state) ){
+           line = NULL; /* signal for strtok to use state */
+           wc ++ ;
+       }
+
+       return wc;
+}
+
+static void *map_thread(void *arg){
 	pid_t  pid;
 	pthread_t  tid;
 
@@ -35,30 +47,74 @@ void *map_thread(void *arg){
         return NULL;
 }
 
+static void trim_nl(char *str){
+   char *p = NULL;
+
+   if (strlen(str)<1 ){
+        return;
+   }
+   p = strrchr(str,'\n');
+   if ( p!=NULL ){
+        *p = '\0';
+   }
+
+   p = strrchr(str,'\r');
+   if ( p!=NULL ){
+        *p = '\0';
+   }
+}
+
 int main(int argc, char **argv){
 
+    char buf[1024];
+    FILE *f;
     int i;
     int n_threads;
     pthread_t *threads;
+    int *sums,total;
 
 #ifdef MAPR_WC_MTRACE
     mtrace();
 #endif
 
-    if ( argc!=2){
+    if ( argc!=3){
         return usage(argv[0],NULL);
     }
-    n_threads = atoi(argv[1]);
+    n_threads = atoi(argv[2]);
     if ( n_threads <= 0 ){
-        fprintf(stderr,"ERROR: Invalid number of threads int(\"%s\") -> %d\n",
-                       argv[1],n_threads);
-        return 1;
+        /* handle zero specialy */
+        if ( strcmp(argv[2],"0") ){
+		fprintf(stderr,"ERROR: Invalid number of threads int(\"%s\") -> %d\n",
+			       argv[2],n_threads);
+		return 1;
+        }
     }
-    threads = (pthread_t*)malloc( sizeof(pthread_t) * n_threads );
-    if ( threads == NULL ){
-         perror("malloc");
+ 
+    if (n_threads > 0){
+            printf("Using %d map threads\n",n_threads);
+	    threads = (pthread_t*)malloc( sizeof(pthread_t) * n_threads );
+	    if ( threads == NULL ){
+		 perror("malloc threads");
+		 return 1;
+	    }
+            sums = (int*)malloc(sizeof(int)*n_threads);
+            if ( sums == NULL ){
+		 perror("malloc sums");
+                 return 1;
+            }
+            
+    } else {
+           printf("Using single threaded model\n");
+    }
+
+    printf("Opening '%s' for reading\n",argv[1]);
+    f=fopen(argv[1],"r");
+    if ( f == NULL ){
+         /* TODO: show, which file failed to open */
+         perror("fopen");
          return 1;
     }
+
 
     for(i=0;i<n_threads;i++){
           if ( pthread_create( threads+i,NULL,map_thread,NULL) ){
@@ -67,6 +123,23 @@ int main(int argc, char **argv){
           }
     }
 
+    total = 0;
+    while( fgets(buf,sizeof(buf)-1,f) !=NULL ){
+           trim_nl(buf);        
+           if ( strlen(buf) == 0 ){
+                continue;
+           }
+           if ( n_threads ){
+              fprintf(stderr,"Multi-threading mode not yet implemented\n");
+              return 1;
+           } else {
+              total += count_words(buf);
+           }
+    }
+
+    fclose(f);f=NULL;
+ 
+
     /* join all threads */
     for(i=0;i<n_threads;i++){
           if ( pthread_join( threads[i],NULL) ){
@@ -74,8 +147,11 @@ int main(int argc, char **argv){
                return 1;
           }
     }
-    printf("All done.\n");
-    free(threads);
+    printf("There are total %d words\n",total);
+    if ( n_threads ){
+	    free(threads); threads = NULL;
+            free(sums); sums = NULL;
+    }
 
 #ifdef MAPR_WC_MTRACE
     muntrace();
